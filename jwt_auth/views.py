183d1077c33f5bbec687fa5b57d8e0c2f7272bb4
Krissy Little <kristenlittle1992@gmail.com>
2020-06-06 10:41:45 +0100

    jwt_auth started

--- jwt_auth/views.py
+++ jwt_auth/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+# pylint: disable=no-member, no-self-use
+from datetime import datetime, timedelta
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from django.conf import settings
+import jwt
 
-# Create your views here.
+
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'Registration Successful'})
+        return Response(serializer.errors, status=422)
