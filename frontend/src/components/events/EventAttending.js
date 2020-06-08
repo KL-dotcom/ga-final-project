8c0c68770b23158d8737259f543f2fb8f495e6f6
Ben Roberts <benroberts67@hotmail.co.uk>
2020-06-08 14:10:44 +0100

    commiting work

--- frontend/src/components/events/EventAttending.js
+++ frontend/src/components/events/EventAttending.js
@@ -5,6 +5,9 @@ const EventAttending = () => {
   return (
     <div className="body">
       <div>EventAttending, checking in</div>
+      <iframe width="420" height="315"
+        src="https://www.youtube.com/embed/tgbNymZ7vqY?autoplay=1">
+      </iframe>
     </div>
 
   )
