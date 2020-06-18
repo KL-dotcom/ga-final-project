a7e89bdd8236518549da95974bd89fbfae72c8e1
Krissy Little <kristenlittle1992@gmail.com>
2020-06-18 15:08:26 +0100

    deploy test

---
+++ frontend/build/precache-manifest.ad61136a0c4263ef5efe3636610ed279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f5f19a502690c6fc6eff488e9cf179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c4aeaeef1886cb293b8",
+    "url": "/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "26d5fd074adbdeda2058",
+    "url": "/static/css/main.1e1ce4c0.chunk.css"
+  },
+  {
+    "revision": "6c4aeaeef1886cb293b8",
+    "url": "/static/js/2.a6de83b7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a6de83b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26d5fd074adbdeda2058",
+    "url": "/static/js/main.3cb83420.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
